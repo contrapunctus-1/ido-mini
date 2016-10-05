6ca002d534ac07128f72a0792c70de9e341ff996
Kashish Sharma <music.kashish@gmail.com>
2016-10-06 02:27:28 +0530

    Various non-code improvements/cleanup

--- ido-mini.el
+++ ido-mini.el
@@ -1,27 +1,32 @@
+;;;; Listen up, fatuous serfs in the lands of Helm and Ido who know
+;;;; not the possibilities!
+;;;; <dramatic pause>
+;;;; <imperiously> Use `ido-mini' in your miserable init.el.
 
-;; ;; thanks to wilfredh -
-;; ;; https://gist.github.com/Wilfred/31e8e0b24e3820c24850920444dd941d
+;;;; helm-mini's convenience of a single command to swich to buffers,
+;;;; open recentf files, and create new buffers, and the informatively
+;;;; colorful list of candidates, but with ido's smarter sorting and
+;;;; snappy speed.
 
-;; (defun wh/ido-switch-buffer-with-filename ()
-;;   "Switch to a buffer with ido, including the filename in the prompt."
-;;   (interactive)
-;;   (let* ((bufs (buffer-list))
-;;          (bufs-with-paths
-;;           (--map (with-current-buffer it
-;;                    (if (buffer-file-name)
-;;                        (format "%s <%s>" (buffer-name) (buffer-file-name))
-;;                      (buffer-name)))
-;;                  bufs))
-;;          (chosen-index
-;;           (-elem-index
-;;            (ido-completing-read
-;;             "Switch to buffer: " bufs-with-paths)
-;;            bufs-with-paths)))
-;;     (switch-to-buffer (nth chosen-index bufs))))
+;;;; Plus, some improvements of its own -
+;;;; 1. If a recentf entry is already visited by a buffer, put the
+;;;;    entry at the end of the candidates list.
+;;;; 2. (temporarily disabled) tweak `im/use-paths' to search and
+;;;;    display not just buffer names but also their file paths,
+;;;;    wherever applicable.
+;;;; 3. (planned) show documentation of functions from smex, akin to
+;;;;    C-j in helm-M-x
 
-;; TODO - colors!
+;;;; No fifty commands to do the work of one, no uninformatively
+;;;; colorless candidates list, no helm slowing down your init or
+;;;; hanging your Emacs, and no more [Display not ready]!
+
+;;;; thanks to wilfredh for the initial code that got me started -
+;;;; https://gist.github.com/Wilfred/31e8e0b24e3820c24850920444dd941d
+
+;; TODO - colors! see source of `helm-buffer--details'
 ;; - unsaved buffers                - buffer-modified-p
-;; - buffers modified outside emacs - verify-visited-file-modtime
+;; - buffers modified outside emacs - verify-visited-file-modtime - helm-buffer-saved-out
 ;; - buffers with deleted files     - buffer-file-name -> file-exists-p
 ;; - and maybe the matched substring in the candidates
 
@@ -30,6 +35,8 @@
 
 ;; add variables to
 ;; - toggle displaying paths with buffer names
+
+;;;; VARIABLES ----
 
 (defface im/unsaved-file
   '((t :inherit font-lock-type-face))
@@ -43,6 +50,7 @@ their file paths.
 
 Users may also find it useful to set this to nil and to enable
 buffer uniquifying via `toggle-uniquify-buffer-names'.")
+
 (defvar im/buffer-list-functions '(im/buffer-names
                                    im/buffers-clean
                                    im/buffers-bury-visible
@@ -50,19 +58,18 @@ buffer uniquifying via `toggle-uniquify-buffer-names'.")
   "List of functions run sequentially over the output
 of `(buffer-list)', with the result of one being the input of the
 next (using `->list'). Each should accept exactly one argument.
-`ido-mini' uses the resulting list for candidates.")
+The resulting list is used by `ido-mini' for completion
+candidates.")
+
 (defvar im/recentf-list-functions '(im/recentf-bury-visited
                                     im/recentf-color)
   "List of functions run sequentially over `recentf-list', with
 the result of one being the input of the next (using `->list').
-Each should accept exactly one argument. `ido-mini' uses the
-resulting list for candidates.")
+Each should accept exactly one argument. The resulting list is
+used by `ido-mini' for completion candidates.")
 
-(defun im/buffer-names (&optional buffer-list)
-  "Return a list of the name of all buffers returned
-by (buffer-list)."
-  (let ((buffer-list (if buffer-list buffer-list (buffer-list))))
-    (-map 'buffer-name buffer-list)))
+;;;; LIBRARY FUNCTIONS ----
+
 (defun ->list (var functions)
   "Assuming FUNCTIONS is a list of functions (FN1 FN2 FN3),
 return the result of (FN3 (FN2 (FN1 VAR)))"
@@ -70,9 +77,12 @@ return the result of (FN3 (FN2 (FN1 VAR)))"
   (dolist (fn functions)
     (setq var (funcall fn var)))
   var)
-;; (->list 1 '((lambda (a) (+ a 1))
-;;             (lambda (a) (+ a 2))
-;;             (lambda (a) (+ a 3)))) ;; => 7
+
+(defun im/buffer-names (&optional buffer-list)
+  "Return a list of the name of all buffers returned
+by (buffer-list)."
+  (let ((buffer-list (if buffer-list buffer-list (buffer-list))))
+    (-map 'buffer-name buffer-list)))
 (defun im/buffers-clean (buffer-names)
   "Remove buffers whose names start with a space."
   (cl-flet ((leading-space-p
@@ -107,6 +117,7 @@ return the result of (FN3 (FN2 (FN1 VAR)))"
          (propertize buffer-name 'face 'italic))
         (t buffer-name))))
    buffer-names))
+
 (defun im/recentf-bury-visited (recentf)
   "Returns the contents of `recentf-list', with files being
 visited by a buffer placed at the end of the list."
@@ -124,6 +135,8 @@ visited by a buffer placed at the end of the list."
 ;;           '(im/buffers-clean im/buffers-bury-visible im/buffers-color))
 ;;   (->list recentf-list '(im/recentf-bury-visited
 ;;                         im/recentf-color))))
+;;;; IDO-MINI ----
+
 (defun ido-mini ()
   "A helm-mini replacement using Ido. Switch to a buffer or a
 recentf entry with ido. If `im/use-paths' is non-nil,
@@ -131,6 +144,9 @@ search for and display the whole file path instead of just the
 file name.
 
 Using ido-vertical in conjunction may be beneficial.
+
+It can be extended conveniently - see the variables
+`im/buffer-list-functions' and `im/recentf-list-functions'.
 
 Based off code from wilfredh -
 https://gist.github.com/Wilfred/31e8e0b24e3820c24850920444dd941d"
@@ -164,13 +180,3 @@ https://gist.github.com/Wilfred/31e8e0b24e3820c24850920444dd941d"
       (find-file (nth (- chosen-index buflength)
                       recentf-sorted)))))
 
-;; pipeline
-
-;;                      recentf-list  -> sorted recentf -> color
-;;                                                             \
-;;                                                            combine -> ido-completing-read -> select from sorted buffer/recentf list
-;;                                                            /
-;; (buffer-list) -> buffer names -> sort -> add paths -> color
-;;                       |           |         |
-;;                (im/buffer-names)  |  (im/add-paths)
-;;                        (im/prep-buffer-list)
